5810b6016fa55b4f53093dc255e18f1a3f72b279
Tomas Novotny <tomas.novotny@synetech.cz>
2020-03-21 23:19:15 +0100

    add gitignore

--- Pods/Target Support Files/Pods-SimpleTimer-SwiftUI-SimpleTimer-SwiftUIUITests/Pods-SimpleTimer-SwiftUI-SimpleTimer-SwiftUIUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SimpleTimer_SwiftUI_SimpleTimer_SwiftUIUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SimpleTimer_SwiftUI_SimpleTimer_SwiftUIUITests
-@end
