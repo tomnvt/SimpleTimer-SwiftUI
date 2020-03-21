73e78377be228331d79b0241a38e0c667a2a4a06
Tomas Novotny <tomas.novotny@synetech.cz>
2020-03-21 22:25:28 +0100

    basic content view setup

--- SimpleTimer-SwiftUI/ContentView.swift
+++ SimpleTimer-SwiftUI/ContentView.swift
@@ -9,13 +9,49 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @ObservedObject var viewModel = ContentViewModel()
+
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Spacer()
+            HStack {
+                Text(self.viewModel.hours)
+                Text(":")
+                Text(self.viewModel.minutes)
+                Text(":")
+                Text(self.viewModel.seconds)
+            }
+            Spacer()
+            Button(action: {
+                self.viewModel.onToggleTimerButtonTap()
+            }) {
+                Text(viewModel.startStopButtonTitle)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+class ContentViewModel: ObservableObject {
+    
+    @Published var hours: String = "01"
+    @Published var minutes: String = "00"
+    @Published var seconds: String = "00"
+
+    @Published var startStopButtonTitle: String = "Start"
+
+    var timerIsRunning = false
+
+    func onToggleTimerButtonTap() {
+        timerIsRunning = !timerIsRunning
+        startStopButtonTitle = timerIsRunning ?
+            "Stop": "Start"
+        hours = "00"
     }
 }
